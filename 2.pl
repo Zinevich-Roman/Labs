f7f33418c44f279e810ff162b38d10f922f0434d
Zinevich-Roman <137852595+Zinevich-Roman@users.noreply.github.com>
2026-05-17 14:01:03 +0800

    Update 2.pl

--- 2.pl
+++ 2.pl
@@ -1,6 +1,11 @@
-% Базовый
+% Вариант 17: Добавить элемент в конец списка
 add_tail([], Element, [Element]).
-
-% Рекурсия
 add_tail([Head|Tail], Element, [Head|NewTail]) :-
     add_tail(Tail, Element, NewTail).
+
+% Вариант 7: Определить, являются ли два элемента соседними
+% Базовый 
+adjacent(X, Y, [X, Y | _]).
+% Рекурсия
+adjacent(X, Y, [_ | Tail]) :- 
+    adjacent(X, Y, Tail).
